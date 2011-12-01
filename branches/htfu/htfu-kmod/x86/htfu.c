99875417c7f90cf0ddcffe5e6ba4be9af49d1625
pfohjo@gmail.com <pfohjo@gmail.com@938be4df-e900-24ff-c56c-49b7c10600cc>
2011-12-01 21:45:05 +0000

    htfu - sc blocking works

--- branches/htfu/htfu-kmod/x86/htfu.c
+++ branches/htfu/htfu-kmod/x86/htfu.c
@@ -109,24 +109,88 @@ int htfu_harden(struct kvm *kvm, uint32_t interrupt){
 
 			memcpy(idt + (kvm->htfu_data.dummy_idt_offset*idt_entry_size), idt + (kvm->htfu_data.idt_offset*idt_entry_size), idt_entry_size);
 
-			//idt[(kvm->htfu_data.idt_offset*idt_entry_size) + 5] &= 0x7F;  //unset present bit
+
 
 			kvm->htfu_data.int_selector=*((u16*) (idt +  (kvm->htfu_data.idt_offset*idt_entry_size) + 2)); //save selector
 			*((u16*) (idt +  (kvm->htfu_data.idt_offset*idt_entry_size) + 2)) = 0; //mangle selector
+
+			//idt[(kvm->htfu_data.idt_offset*idt_entry_size) + 5] &= 0x7F;  //unset present bit
 
 			kvm_write_guest_virt_system(sregs.idt.base,idt,(unsigned int)(sregs.idt.limit + 1),vcpu,&error);
 
 			kfree(idt);
 		}
 	}
+	printk(KERN_INFO "kvm:htfu: done.\n");
 
 	return 0;
 }
 
 int htfu_unharden(struct kvm *kvm){
-	printk(KERN_INFO "kvm:htfu: htfu_unharden \n");
+	int i;
+	struct kvm_sregs sregs;
+	struct kvm_vcpu *vcpu;
+	u64 idt_base;
+	u8 *idt;
+	u32 error;
 
+	if(kvm->htfu_data.hardened == 0){
+		printk(KERN_INFO "kvm:htfu: System is not hardened.\n");
+		return 1;
+	}
+
+	printk(KERN_INFO "kvm:htfu: Unhardening system...\n");
+
+	//unset #GP trap
 	kvm->htfu_data.hardened = 0;
+	kvm_for_each_vcpu(i, vcpu, kvm){
+		vcpu_load(vcpu);
+		kvm_x86_ops->update_trap_exceptions(vcpu);
+		vcpu_put(vcpu);
+	}
+
+
+	//code to uncause sysenter to cause #GP
+	kvm_for_each_vcpu(i, vcpu, kvm){
+		vcpu_load(vcpu);
+		kvm_x86_ops->set_msr(vcpu, MSR_IA32_SYSENTER_CS, kvm->htfu_data.sysenter_cs_val);
+		vcpu_put(vcpu);
+	}
+
+	//code to uncause syscall to cause #UD (64 bit ubuntu)
+	kvm_for_each_vcpu(i, vcpu, kvm){
+		vcpu_load(vcpu);
+		kvm_set_msr_common(vcpu, MSR_EFER, kvm->htfu_data.efer_val);
+		vcpu_put(vcpu);
+	}
+
+	//code to cause int to cause #GP
+	idt_base = 0;
+
+	kvm_for_each_vcpu(i, vcpu, kvm){
+		vcpu_load(vcpu);
+		kvm_arch_vcpu_ioctl_get_sregs(vcpu,&sregs);
+		vcpu_put(vcpu);
+
+		if(sregs.idt.base != idt_base){
+			idt_base = sregs.idt.base;
+
+			idt = kmalloc(sregs.idt.limit + 1,GFP_KERNEL);
+			memset(idt,0,sregs.idt.limit + 1);
+
+			kvm_read_guest_virt_system(sregs.idt.base,idt,(unsigned int)(sregs.idt.limit + 1),vcpu,&error);
+
+
+			memcpy(idt + (kvm->htfu_data.idt_offset*idt_entry_size), idt + (kvm->htfu_data.dummy_idt_offset*idt_entry_size), idt_entry_size);
+
+
+			kvm_write_guest_virt_system(sregs.idt.base,idt,(unsigned int)(sregs.idt.limit + 1),vcpu,&error);
+
+			kfree(idt);
+		}
+	}
+	printk(KERN_INFO "kvm:htfu: done.\n");
+
 	return 0;
 }
 
@@ -154,23 +218,49 @@ int htfu_unblock_sc(struct kvm *kvm, uint32_t sc_nr){
 	return 0;
 }
 
-void htfu_handle_gp(struct kvm_vcpu *vcpu, struct kvm_run *kvm_run){
-	//printk(KERN_INFO "#GP caught\n");
+bool do_syscall(struct kvm_vcpu *vcpu){
+	unsigned long sc_nr = kvm_register_read(vcpu, VCPU_REGS_RAX);
 
-	if(is_sysenter(vcpu) || is_sysexit(vcpu)){//sysenter/sysexit
-		//printk(KERN_INFO "sysenter_sysreturn trapped\n");
+	if(sc_nr == 127 || sc_nr == 128){
+		printk(KERN_INFO "kvm:htfu: Blocking system call %lu\n",sc_nr);
+		return false;
+	}
+	else
+		return true;
+}
+
+void htfu_handle_gp(struct kvm_vcpu *vcpu, struct kvm_run *kvm_run){
+
+	if(is_sysenter(vcpu)){
+		if(do_syscall(vcpu)){
+			if (emulate_instruction(vcpu, 0, 0, 0) != EMULATE_DONE){
+				kvm_clear_exception_queue(vcpu);
+				kvm_clear_interrupt_queue(vcpu);
+				kvm_queue_exception_e(vcpu,GP_VECTOR,kvm_run->ex.error_code);
+			}
+		}
+		else{
+			kvm_queue_exception(vcpu, UD_VECTOR);
+		}
+	}
+	else if(is_sysexit(vcpu)){
 		if (emulate_instruction(vcpu, 0, 0, 0) != EMULATE_DONE){
-			printk(KERN_INFO "prob emulating sysenter_sysreturn\n");
 			kvm_clear_exception_queue(vcpu);
 			kvm_clear_interrupt_queue(vcpu);
 			kvm_queue_exception_e(vcpu,GP_VECTOR,kvm_run->ex.error_code);
 		}
+
 	}
-	else if(is_int(vcpu)){//int
-		printk(KERN_INFO "int trapped\n");
-		kvm_clear_exception_queue(vcpu);
-		kvm_clear_interrupt_queue(vcpu);
-		kvm_queue_interrupt(vcpu,vcpu->kvm->htfu_data.dummy_idt_offset,true);
+	else if(is_int(vcpu)){
+		printk(KERN_INFO "int trapped %lu\n",kvm_register_read(vcpu, VCPU_REGS_RAX));
+		if(do_syscall(vcpu)){
+			kvm_clear_exception_queue(vcpu);
+			kvm_clear_interrupt_queue(vcpu);
+			kvm_queue_interrupt(vcpu,vcpu->kvm->htfu_data.dummy_idt_offset,true);
+		}
+		else{
+			kvm_queue_exception(vcpu, UD_VECTOR);
+		}
 	}
 	else{
 		kvm_clear_exception_queue(vcpu);
